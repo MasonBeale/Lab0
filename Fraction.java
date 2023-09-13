b994b407b448d17ef5460ef1c6cbe325238f0515
mbeal_sgp6gtp <mbeal_sgp6gtp@MasonBealeLaptop.lan>
2023-09-12 22:49:10 -0400

    initial Fraction.java

--- Fraction.java
+++ Fraction.java
@@ -1,1 +1,51 @@
+package labspack;
+
+import labspack.Fraction;
+import labspack.INumber;
+
+public class Fraction implements INumber<Fraction> {
+	//fields
+	private int numerator;
+	private int denominator;
+	
+	/**
+	 * The constructor for making a fraction
+	 * @param num The numerator as an int
+	 * @param den The denominator as an int
+	 */
+	public Fraction(int num, int den){
+		numerator = num;
+		denominator = den;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Fraction plus(Fraction input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Fraction minus(Fraction input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Fraction divide(Fraction input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Fraction multiply(Fraction input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void print() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
 
